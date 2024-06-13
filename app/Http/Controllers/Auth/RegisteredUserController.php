1888625704842d7c28b74283a542fb205c329d3d
fraberret <f.berretta02@gmail.com>
2024-06-13 17:26:17 +0200

    add restaurant validation (ERROR: no save in restaurant table)

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
+use App\Models\Restaurant;
 use App\Models\User;
 use App\Providers\RouteServiceProvider;
 use Illuminate\Auth\Events\Registered;
@@ -30,10 +31,16 @@ class RegisteredUserController extends Controller
      */
     public function store(Request $request): RedirectResponse
     {
+        /* dd($request); */
         $request->validate([
             'name' => ['required', 'string', 'max:255'],
-            'email' => ['required', 'string', 'lowercase', 'email', 'max:255', 'unique:'.User::class],
+            'email' => ['required', 'string', 'lowercase', 'email', 'max:255', 'unique:' . User::class],
             'password' => ['required', 'confirmed', Rules\Password::defaults()],
+            'restaurant_name' => ['required', 'string', 'min:5', 'max:50'],
+            'address' => ['nullable', 'string', 'min:5', 'max:255'],
+            'telephone_number' => ['nullable', 'string', 'size:13'],
+            'logo' => ['nullable', 'image', 'max:500'],
+            'piva' => ['nullable', 'string', 'size:11'],
         ]);
 
         $user = User::create([
@@ -41,6 +48,15 @@ class RegisteredUserController extends Controller
             'email' => $request->email,
             'password' => Hash::make($request->password),
         ]);
+
+        $restaurant = new Restaurant;
+        $restaurant->name = $request->restaurant_name;
+        /* 'name' => $request->restaurant_name,
+            'address' => $request->address,
+            'telephone_number' => $request->telephone_number,
+            'logo' => $request->logo,
+            'piva' => $request->piva, */
+
 
         event(new Registered($user));
 
