ec609f52229ebf550ca023df7d39b355381e6acf
fraberret <f.berretta02@gmail.com>
2024-06-14 14:36:55 +0200

    fix error messages in update-profile form

--- resources/views/profile/partials/update-profile-information-form.blade.php
+++ resources/views/profile/partials/update-profile-information-form.blade.php
@@ -18,84 +18,80 @@
         @method('patch')
 
         <div class="mb-2">
-            <label for="name">{{__('Name')}}</label>
-            <input class="form-control" type="text" name="name" id="name" autocomplete="name" value="{{old('name', $user->name)}}" required autofocus>
+            <label for="name">{{ __('Name') }}</label>
+            <input class="form-control" type="text" name="name" id="name" autocomplete="name"
+                value="{{ old('name', $user->name) }}" required autofocus>
             @error('name')
-            <span class="invalid-feedback" role="alert">
-                <strong>{{ $errors->get('name')}}</strong>
-            </span>
+                <div class="alert alert-danger">{{ $message }}</div>
             @enderror
         </div>
 
         <div class="mb-2">
             <label for="email">
-                {{__('Email') }}
+                {{ __('Email') }}
             </label>
 
-            <input id="email" name="email" type="email" class="form-control" value="{{ old('email', $user->email)}}" required autocomplete="username" />
+            <input id="email" name="email" type="email" class="form-control"
+                value="{{ old('email', $user->email) }}" required autocomplete="username" />
 
             @error('email')
-            <span class="alert alert-danger mt-2" role="alert">
-                <strong>{{ $errors->get('email')}}</strong>
-            </span>
+                <div class="alert alert-danger">{{ $message }}</div>
             @enderror
 
-            @if ($user instanceof \Illuminate\Contracts\Auth\MustVerifyEmail && ! $user->hasVerifiedEmail())
-            <div>
-                <p class="text-sm mt-2 text-muted">
-                    {{ __('Your email address is unverified.') }}
+            @if ($user instanceof \Illuminate\Contracts\Auth\MustVerifyEmail && !$user->hasVerifiedEmail())
+                <div>
+                    <p class="text-sm mt-2 text-muted">
+                        {{ __('Your email address is unverified.') }}
 
-                    <button form="send-verification" class="btn btn-outline-dark">
-                        {{ __('Click here to re-send the verification email.') }}
-                    </button>
-                </p>
+                        <button form="send-verification" class="btn btn-outline-dark">
+                            {{ __('Click here to re-send the verification email.') }}
+                        </button>
+                    </p>
 
-                @if (session('status') === 'verification-link-sent')
-                <p class="mt-2 text-success">
-                    {{ __('A new verification link has been sent to your email address.') }}
-                </p>
-                @endif
-            </div>
+                    @if (session('status') === 'verification-link-sent')
+                        <p class="mt-2 text-success">
+                            {{ __('A new verification link has been sent to your email address.') }}
+                        </p>
+                    @endif
+                </div>
             @endif
         </div>
 
         <div class="mb-2">
             <label for="restaurant_name">Restaurant Name</label>
-            <input class="form-control" type="text" name="restaurant_name" id="restaurant_name" autocomplete="restaurant_name" value="{{old('restaurant_name', $user->restaurant->name)}}" required autofocus>
+            <input class="form-control" type="text" name="restaurant_name" id="restaurant_name"
+                autocomplete="restaurant_name" value="{{ old('restaurant_name', $user->restaurant->name) }}" required
+                autofocus>
             @error('restaurant_name')
-            <span class="invalid-feedback" role="alert">
-                <strong>{{ $errors->get('restaurant_name')}}</strong>
-            </span>
+                <div class="alert alert-danger">{{ $message }}</div>
             @enderror
         </div>
 
         <div class="mb-2">
             <label for="telephone_number">Telephone Number</label>
-            <input class="form-control" type="text" name="telephone_number" id="telephone_number" autocomplete="telephone_number" value="{{old('telephone_number', $user->restaurant->telephone_number)}}" required autofocus>
+            <input class="form-control" type="text" name="telephone_number" id="telephone_number"
+                autocomplete="telephone_number"
+                value="{{ old('telephone_number', $user->restaurant->telephone_number) }}" required autofocus>
             @error('telephone_number')
-            <span class="invalid-feedback" role="alert">
-                <strong>{{ $errors->get('telephone_number')}}</strong>
-            </span>
+                <div class="alert alert-danger">{{ $message }}</div>
             @enderror
         </div>
 
         <div class="mb-2">
             <label for="address">Address</label>
-            <input class="form-control" type="text" name="address" id="address" autocomplete="address" value="{{old('address', $user->restaurant->address)}}" required autofocus>
+            <input class="form-control" type="text" name="address" id="address" autocomplete="address"
+                value="{{ old('address', $user->restaurant->address) }}" required autofocus>
             @error('address')
-            <span class="invalid-feedback" role="alert">
-                <strong>{{ $errors->get('address')}}</strong>
-            </span>
+                <div class="alert alert-danger">{{ $message }}</div>
             @enderror
         </div>
 
         <div class="mb-2">
             <label for="piva">P.Iva</label>
-            <input class="form-control" type="text" name="piva" id="piva" autocomplete="piva" value="{{old('piva', $user->restaurant->piva)}}" required autofocus>
+            <input class="form-control" type="text" name="piva" id="piva" autocomplete="piva"
+                value="{{ old('piva', $user->restaurant->piva) }}" required autofocus>
             @error('piva')
-            <span class="invalid-feedback" role="alert">
-                <strong>{{ $errors->get('piva')}}</strong>
-            </span>
+                <div class="alert alert-danger">{{ $message }}</div>
             @enderror
         </div>
 
@@ -103,15 +99,15 @@
             <button class="btn btn-primary" type="submit">{{ __('Save') }}</button>
 
             @if (session('status') === 'profile-updated')
-            <script>
-                const show = true;
-                setTimeout(() => show = false, 2000)
-                const el = document.getElementById('profile-status')
-                if (show) {
-                    el.style.display = 'block';
-                }
-            </script>
-            <p id='profile-status' class="fs-5 text-muted">{{ __('Saved.') }}</p>
+                <script>
+                    const show = true;
+                    setTimeout(() => show = false, 2000)
+                    const el = document.getElementById('profile-status')
+                    if (show) {
+                        el.style.display = 'block';
+                    }
+                </script>
+                <p id='profile-status' class="fs-5 text-muted">{{ __('Saved.') }}</p>
             @endif
         </div>
     </form>
