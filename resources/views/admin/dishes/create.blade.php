eae277ed22a69413f08d4ae58132b4dd36f6f450
Daniele <danielecolaci@outlook.com>
2024-06-16 21:13:37 +0200

    Fix Create form layout, add Price input

--- resources/views/admin/dishes/create.blade.php
+++ resources/views/admin/dishes/create.blade.php
@@ -1,13 +1,12 @@
 @extends('layouts.admin')
 
 @section('content')
-    <div class="container">
+    <div class="container my-5">
         <form action="{{ route('admin.dishes.store') }}" method="post" enctype="multipart/form-data">
             @csrf
 
-
             <div class="mb-3">
-                <label for="name" class="form-label">name:</label>
+                <label for="name" class="form-label">Name</label>
                 <input type="text" class="form-control @error('name') is-invalid @enderror" name="name" id="name"
                     value="{{ old('name') }}" />
                 @error('name')
@@ -18,7 +17,7 @@
             </div>
 
             <div class="mb-3">
-                <label for="cover_image" class="form-label">cover_image:</label>
+                <label for="cover_image" class="form-label">Cover Image</label>
                 <input type="file" class="form-control @error('cover_image') is-invalid @enderror" name="cover_image"
                     id="cover_image" />
                 @error('cover_image')
@@ -29,33 +28,32 @@
             </div>
 
             <div class="mb-3">
-                {{-- <input type="checkbox" class="form-check-input" id="visible" name="visible"
-                    {{ old('visible') ? 'checked' : '' }} value="true">
-                <label class="form-check-label" for="visible">Visible</label>
+                <label for="price" class="form-label">Price</label>
+                <input type="number" step="0.01" class="form-control @error('price') is-invalid @enderror"
+                    name="price" id="price" value="{{ old('price') }}" />
+                @error('price')
+                    <div class="text-danger py-2">
+                        {{ $message }}
+                    </div>
+                @enderror
+            </div>
+
+            <div class="mb-3">
+                <label for="visible" class="form-label">Visible</label>
+                <select class="form-select form-select-sm" name="visible" id="visible">
+                    <option selected disabled>Select one</option>
+                    <option value="1">True</option>
+                    <option value="0">False</option>
+                </select>
                 @error('visible')
                     <div class="text-danger py-2">
                         {{ $message }}
                     </div>
-                @enderror --}}
-
-                <div class="mb-3">
-                    <label for="visible" class="form-label">Visible</label>
-                    <select class="form-select form-select-sm" name="visible" id="visible">
-                        <option selected disabled>Select one</option>
-                        <option value="1">True</option>
-                        <option value="0">False</option>
-                    </select>
-                    @error('visible')
-                        <div class="text-danger py-2">
-                            {{ $message }}
-                        </div>
-                    @enderror
-                </div>
-
+                @enderror
             </div>
 
             <div class="mb-3">
-                <label for="description" class="form-label">Description:</label>
+                <label for="description" class="form-label">Description</label>
                 <textarea class="form-control @error('description') is-invalid @enderror" name="description" id="description"
                     rows="5">{{ old('description') }}</textarea>
                 @error('description')
@@ -65,9 +63,14 @@
                 @enderror
             </div>
 
-            <button type="submit" class="btn btn-primary">
-                Create
-            </button>
+            <div class="d-flex justify-content-between">
+                <a href="{{ route('admin.dishes.index') }}" class="btn btn-secondary">
+                    <i class="fa fa-arrow-left" aria-hidden="true"></i> Back
+                </a>
+                <button type="submit" class="btn btn-success">
+                    <i class="fa fa-check" aria-hidden="true"></i> Create
+                </button>
+            </div>
 
         </form>
     </div>
