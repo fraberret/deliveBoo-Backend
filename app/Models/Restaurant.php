743445a35f874c81ce327e76358db58870896fd7
Nico-Serra <nico.serra000@gmail.com>
2024-06-17 10:21:05 +0200

    import Dish and Restaruant in your Model

--- app/Models/Restaurant.php
+++ app/Models/Restaurant.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 use App\Models\User;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasMany;
-
+use App\Models\Dish;
 
 class Restaurant extends Model
 {
