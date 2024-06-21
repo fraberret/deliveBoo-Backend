f0c91e95f32ac3b6ddd0d32cd453d33d43d0d138
Daniele <danielecolaci@outlook.com>
2024-06-21 14:30:03 +0200

    Create Order Model, migration, controller, seeder

---
+++ app/Models/Order.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use App\Models\Restaurant;
+
+
+class Order extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'restaurant_id',
+        'customer_name',
+        'customer_last_name',
+        'customer_address',
+        'customer_telephone',
+        'customer_email',
+        'date',
+        'total',
+    ];
+
+    /**
+     * The roles that belong to the Order
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsToMany
+     */
+    /**
+     * Get the user that owns the Order
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function restaurant(): BelongsTo
+    {
+        return $this->belongsTo(Restaurant::class);
+    }
+
+}
