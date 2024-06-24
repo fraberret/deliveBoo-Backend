6f497f712e4ccd7a33f6b0c1ad2ceaefcc1e8bf1
RiccardoImperiale <riccardoimperiale.dev@gmail.com>
2024-06-24 10:12:07 +0200

    add request to validate makePayment request

--- app/Http/Controllers/Api/PaymentController.php
+++ app/Http/Controllers/Api/PaymentController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\PaymentRequest;
 use Braintree\Gateway;
 use Illuminate\Http\Request;
 
@@ -31,7 +32,7 @@ class PaymentController extends Controller
         ]);
     }
 
-    public function makePayment(Request $request)
+    public function makePayment(PaymentRequest $request)
     {
         $result = $this->gateway->transaction()->sale([
             'amount' => $request->amount,
