6484d029bf506091fa9ebd636d58067cb2278842
RiccardoImperiale <riccardoimperiale.dev@gmail.com>
2024-06-26 17:40:59 +0200

    add welcome page style

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,32 +1,32 @@
-@extends('layouts.app')
+@extends('layouts.welcome')
 
 @section('title', 'Home')
 
 @section('content')
+    <div class="welcome">
+        <img class="gif" src="{{ asset('img/passing3.gif') }}" alt="app logo">
 
-    <div class="jumbotron p-5 mb-4">
-        <div class="container py-5">
-            <div class="d-flex align-items-center">
-                <h2 class="display-3 text-secondary fw-thin me-3">
-                    Welcome to
-                </h2>
 
-                <div class="logo_laravel ps-2">
-                    <img width="300" src="{{ asset('img/logo-deliveboo-back.png') }}" alt="app logo">
-                </div>
+
+        <div class="text">
+            <h1>
+                Welcome to DeliveBoo
+            </h1>
+            <div class="mt-4 actions">
+                @guest
+                    <a href="{{ route('login') }}" class="btn_primary">{{ __('Login') }}</a>
+                    <a href="{{ route('register') }}" class="btn_dark">{{ __('Register') }}</a>
+                @else
+                    <a href="{{ url('dashboard') }}" class="btn_primary">{{ __('Dashboard') }}</a>
+                @endguest
             </div>
-
-            <p class="col-md-8 fs-4">
-                DeliveBoo is a home food buying and selling platform.
-                Access the page to insert your menu and manage orders and sales statistics.
-            </p>
-            @guest
-                <a href="{{ route('login') }}" class="btn btn-secondary">{{ __('Login') }}</a>
-                <a href="{{ route('register') }}" class="btn btn-secondary">{{ __('Register') }}</a>
-            @else
-                <a href="{{ url('dashboard') }}" class="btn btn-secondary">{{ __('Dashboard') }}</a>
-            @endguest
         </div>
+
+        {{-- <p class="container m-auto">
+            DeliveBoo is a home food buying and selling platform.
+            Access the page to insert your menu and manage orders and sales statistics.
+        </p> --}}
+
     </div>
 
 @endsection
