50e93ec36d1372cfea821d56d1f747833ee0188a
nicodellolio <151004233+nicodellolio@users.noreply.github.com>
2024-06-26 12:28:30 +0200

    add info to the dashboard

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -2,13 +2,23 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Dish;
+use App\Models\Order;
+use App\Models\Restaurant;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class DashboardController extends Controller
 {
     public function index(){
 
+        $userId = Auth::id();
+        $restaurant = Restaurant::where('user_id', $userId)->first();
 
-        return view('dashboard');
+        $dishes = Dish::orderByDesc('id')->where('restaurant_id', $restaurant->id)->get();
+
+        $orders = Order::orderByDesc('created_at')->where('restaurant_id', $restaurant->id)->get();
+
+        return view('dashboard', compact('userId', 'restaurant', 'dishes', 'orders'));
     }
 }
--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,29 +1,92 @@
 @extends('layouts.admin')
 
-@section('title', "Dashboard")
+@section('title', 'Dashboard')
 
 
 @section('content')
     <div class="container">
-        <h2 class="fs-4 text-secondary my-4">
-            {{ __('Dashboard') }}
-        </h2>
-        <div class="row justify-content-center">
-            <div class="col">
-                <div class="card">
-                    <div class="card-header">{{ __('User Dashboard') }}</div>
 
-                    <div class="card-body">
-                        @if (session('status'))
-                            <div class="alert alert-success" role="alert">
-                                {{ session('status') }}
-                            </div>
-                        @endif
+        <div class="banner d-flex">
+            <div class="text">
+                <h6>{{ __('User Dashboard') }}</h6>
 
-                        {{ __('You are logged in!') }}
-                    </div>
+                @if (session('status'))
+                    {{ session('status') }}
+                @endif
+
+                <h3 class="fw-bold"> {{ __('You are logged in!') }}</h3>
+
+                
+                <h6 class="dash-info mt-5">
+                    <a class="text-decoration-none" style="color: var(--boo-primary)" href="{{ route('admin.orders.index') }}">
+                        You have {{ count($orders) }} new orders
+                    </a>
+                </h6>
+                <h6 class="dash-info mt-5">
+                    You currently have {{ count($dishes) }} dishes
+                </h6>
+                <div class="dishes-box-dash d-flex">
+                    <a href="{{ route('admin.dishes.index') }}" class="btn btn-transparent border">
+                        See all
+                    </a>
+                    <a href="{{ route('admin.dishes.create') }}" class="btn btn-transparent border ms-3">
+                        Create a new one
+                    </a>
                 </div>
+                <h6 class="dash-info mt-5">
+                    <a class="text-decoration-none" style="color: var(--boo-primary)"
+                        href="{{ route('admin.statistics.index') }}">
+                        see the statistics
+                    </a>
+                </h6>
+
+
             </div>
+            <div class="info">
+                <ul class="list-unstyled text-center">
+                    <li class="fw-semibold fs-4">
+
+                        <div class="restaurant_image mb-2">
+                            @if (Str::startsWith($restaurant->logo, 'https://'))
+                                <img class="rounded-3" width="100" src="{{ $restaurant->logo }}" alt="">
+                            @elseif (Str::startsWith($restaurant->logo, '/img/'))
+                                <img class="rounded-3" width="100" src="{{ asset($restaurant->logo) }}" alt="">
+                            @else
+                                <img class="rounded-3" width="100" src="{{ asset('storage/' . $restaurant->logo) }}" alt="">
+                            @endif
+                        </div>
+
+                        {{ $restaurant->name }}
+                    </li>
+                    <li>
+                        @foreach ($restaurant->cousines as $cousine)
+                            <small class="badge bg-light text-dark fw-normal ms-2 mb-2">{{ $cousine->name }}</small>
+                        @endforeach
+                    </li>
+                    <li>
+                        {{ $restaurant->telephone_number }}
+                    </li>
+                    <li>
+                        {{ $restaurant->address }}
+                    </li>
+                    <li>
+                        VAT: {{ $restaurant->piva }}
+                    </li>
+                    <hr>
+                    <li>
+                        {{ $restaurant->user->name }}
+                    </li>
+                    <li>
+                        {{ $restaurant->user->email }}
+                    </li>
+                </ul>
+
+            </div>
+
         </div>
+
+
+
+
     </div>
 @endsection
