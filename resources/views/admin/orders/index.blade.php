427e0355aad50455f5bcb7ab7214e1accacf9c52
RiccardoImperiale <riccardoimperiale.dev@gmail.com>
2024-06-26 15:44:46 +0200

    fix orders page style

--- resources/views/admin/orders/index.blade.php
+++ resources/views/admin/orders/index.blade.php
@@ -11,27 +11,27 @@
                 <h2>See all your orders here!</h2>
             </div>
         </div>
-        <div class="dishes">
-            <h2 class="mb-3">{{ $restaurant->name }}</h2>
+        <div class="orders">
+            <h2>{{ $restaurant->name }}</h2>
             <div class="cols_heading">
                 <div class="name">Customer Name</div>
-                <div class="name">Date</div>
-                <div class="name">Total Price</div>
+                <div class="date">Date</div>
+                <div class="price">Total Price</div>
                 <div class="actions">Actions</div>
             </div>
             <div class="rows">
                 @forelse ($orders as $order)
-                    <div class="dish">
+                    <div class="order">
                         <div class="name">{{ $order->customer_name }} {{ $order->customer_lastname }}</div>
-                        <div class="created_at">{{ $order->created_at }}</div>
+                        <div class="date">{{ $order->created_at }}</div>
                         <div class="price">{{ $order->total }} €</div>
-                        <div class="actions text-start">
+                        <div class="actions">
                             <a href="{{ route('admin.orders.show', $order) }}"><img width="23"
-                                    src="{{ asset('img/icons/eye.png') }}" alt="eye icon" class="mobile_hidden"></a>
+                                    src="{{ asset('img/icons/eye.png') }}" alt="eye icon"></a>
                         </div>
                     </div>
                 @empty
-                    <div class="no_dishes">
+                    <div class="no_orders">
                         Sorry, no orders to show.
                     </div>
                 @endforelse
