f0508fd651fbd06ac319148fcb8f77dadf9d6210
RiccardoImperiale <riccardoimperiale.dev@gmail.com>
2024-06-27 12:44:01 +0200

    fix style on orders table

--- resources/views/admin/orders/index.blade.php
+++ resources/views/admin/orders/index.blade.php
@@ -7,18 +7,25 @@
         @include('admin.partials.session-message')
         <div class="banner">
             <div class="text">
-                <h6>all orders</h6>
+                <h6>orders</h6>
                 <h2>See all your orders here!</h2>
             </div>
         </div>
         <div class="orders">
             @if (count($orders) > 0)
-                <h2>{{ $restaurant->name }}</h2>
+                <div class="title mb-5 d-flex justify-content-between align-items-center">
+                    <h2>{{ $restaurant->name }}</h2>
+                    <div class="total_orders">
+                        <div class="d-flex align-items-center">
+                            <span>total orders:</span> <span>{{ count($orders) }}</span>
+                        </div>
+                    </div>
+                </div>
                 <div class="cols_heading">
                     <div class="name">Customer Name</div>
                     <div class="date">Date</div>
                     <div class="price">Total Price</div>
-                    <div class="actions">Actions</div>
+                    <div class="actions">Review Order</div>
                 </div>
             @endif
             <div class="rows">
@@ -28,8 +35,9 @@
                         <div class="date">{{ $order->created_at }}</div>
                         <div class="price">{{ $order->total }} €</div>
                         <div class="actions">
-                            <a href="{{ route('admin.orders.show', $order) }}"><img width="23"
-                                    src="{{ asset('img/icons/eye.png') }}" alt="eye icon"></a>
+                            <a href="{{ route('admin.orders.show', $order) }}">
+                                <img width="23" src="{{ asset('img/icons/eye.png') }}" alt="eye icon"><span>View</span>
+                            </a>
                         </div>
                     </div>
                 @empty
