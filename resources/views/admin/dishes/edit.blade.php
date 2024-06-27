920981f3e247afe13e74df0d9410a31724d377b1
Daniele <danielecolaci@outlook.com>
2024-06-27 12:52:00 +0200

    Fix Dishes Edit Form

--- resources/views/admin/dishes/edit.blade.php
+++ resources/views/admin/dishes/edit.blade.php
@@ -3,104 +3,131 @@
 @section('title', "Edit $dish->name")
 
 @section('content')
-    <div class="container my-5">
-        <form action="{{ route('admin.dishes.update', $dish) }}" method="post" enctype="multipart/form-data">
+    <div class="container my-5" style="margin-bottom">
+        <form action="{{ route('admin.dishes.update', $dish) }}" method="post" enctype="multipart/form-data" class="form"
+            style="max-width: 1140px">
             @csrf
             @method('PUT')
 
-            <div class="mb-3">
-                <label for="name" class="form-label">{{ __('Name*') }}</label>
-                <input type="text" class="form-control @error('name') is-invalid @enderror" name="name" id="name"
-                    value="{{ old('name', $dish->name) }}" minlength="5" maxlength="100" />
-                @error('name')
-                    <div class="text-danger py-2">
-                        {{ $message }}
-                    </div>
-                @enderror
-            </div>
-
-            <div class="mb-3 d-flex align-items-center">
-                <div class="me-3 ">
-                    @if (Str::startsWith($dish->cover_image, 'https://'))
-                        <img class="rounded-circle" width="140" src="{{ $dish->cover_image }}"
-                            alt="{{ $dish->name }} image">
-                    @elseif (Str::startsWith($dish->cover_image, '/img'))
-                        <img class="rounded-circle" width="140" src="{{ asset($dish->cover_image) }}"
-                            alt="{{ $dish->name }} image">
-                    @else
-                        <img class="rounded-circle" width="140" src="{{ asset('storage/' . $dish->cover_image) }}"
-                            alt="{{ $dish->name }} image">
-                    @endif
+            <div class="top">
+                <div class="title">
+                    <h5>{{ __('Edit Dish') }}</h5>
+                    <hr style="width: 70%">
+                    <img src="{{ asset('img/logo-emblem.png') }}" alt="">
                 </div>
+                <div class="inputs">
+                    <div class="form_input">
+                        <label for="name" class="form-label">{{ __('Name*') }}</label>
+                        <input type="text" class="form-control @error('name') is-invalid @enderror" name="name"
+                            id="name" value="{{ old('name', $dish->name) }}" minlength="5" maxlength="100" />
+                        @error('name')
+                            <div class="text-danger py-2">
+                                {{ $message }}
+                            </div>
+                        @enderror
+                    </div>
 
-                <div class="flex-grow-1">
-                    <label for="cover_image" class="form-label">{{ __('Cover Image') }}</label>
-                    <input type="file" class="form-control @error('cover_image') is-invalid @enderror" name="cover_image"
-                        id="cover_image" />
-                    @error('cover_image')
-                        <div class="text-danger py-2">
-                            {{ $message }}
+                    <div class="form_input d-flex align-items-center">
+                        <div class="me-3 ">
+                            @if (Str::startsWith($dish->cover_image, 'https://'))
+                                <img class="rounded-circle" width="140" src="{{ $dish->cover_image }}"
+                                    alt="{{ $dish->name }} image">
+                            @elseif (Str::startsWith($dish->cover_image, '/img'))
+                                <img class="rounded-circle" width="140" src="{{ asset($dish->cover_image) }}"
+                                    alt="{{ $dish->name }} image">
+                            @else
+                                <img class="rounded-circle" width="140"
+                                    src="{{ asset('storage/' . $dish->cover_image) }}" alt="{{ $dish->name }} image">
+                            @endif
                         </div>
-                    @enderror
-                </div>
-            </div>
 
-            <div class="mb-3">
-                <label for="price" class="form-label">{{ __('Price*') }}</label>
-                <input type="number" step="0.01" class="form-control @error('price') is-invalid @enderror"
-                    name="price" id="price" min='0' value="{{ old('price', $dish->price) }}" />
-                @error('price')
-                    <div class="text-danger py-2">
-                        {{ $message }}
-                    </div>
-                @enderror
-            </div>
-
-            <div class="mb-3 d-flex gap-3">
-                <label for="visible" class="form-label">{{ __('Visible') }}</label>
-                <div class="form-check form-switch">
-                    <input type="hidden" name="visible" value="0">
-                    <input class="form-check-input" type="checkbox" name="visible" id="visible" value="1"
-                        {{ old('visible', $dish->visible) ? 'checked' : '' }}>
-                    @error('visible')
-                        <div class="text-danger py-2">
-                            {{ $message }}
+                        <div class="form_input">
+                            <label for="cover_image" class="form-label">{{ __('Cover Image') }}</label>
+                            <div class="custom-file-input-wrapper d-flex align-items-center gap-3">
+                                <input type="file" class="form-control @error('cover_image') is-invalid @enderror"
+                                    name="cover_image" id="cover_image" />
+                                <label for="cover_image" class="btn btn-primary custom-file-label">
+                                    Choose file
+                                </label>
+                                <span id="file-name" class="file-name">No file chosen</span>
+                            </div>
+                            @error('cover_image')
+                                <div class="text-danger py-2">
+                                    {{ $message }}
+                                </div>
+                            @enderror
                         </div>
-                    @enderror
+                    </div>
+                    <script>
+                        document.getElementById('cover_image').addEventListener('change', function() {
+                            var fileName = this.files[0].name;
+                            document.getElementById('file-name').textContent = fileName;
+                        });
+                    </script>
+
+                    <div class="form_input">
+                        <label for="price" class="form-label">{{ __('Price*') }}</label>
+                        <input type="number" step="1.00" class="form-control @error('price') is-invalid @enderror"
+                            name="price" id="price" min='0' value="{{ old('price', $dish->price) }}" />
+                        @error('price')
+                            <div class="text-danger py-2">
+                                {{ $message }}
+                            </div>
+                        @enderror
+                    </div>
+
+                    <div class="form_input mb-5">
+                        <label for="visible" class="form-label">{{ __('Visible') }}</label>
+                        <div class="form-check form-switch">
+                            <input type="hidden" name="visible" value="0">
+                            <input class="form-check-input" type="checkbox" name="visible" id="visible" value="1"
+                                {{ old('visible', $dish->visible) ? 'checked' : '' }}>
+                            @error('visible')
+                                <div class="text-danger py-2">
+                                    {{ $message }}
+                                </div>
+                            @enderror
+                        </div>
+                    </div>
+
+                    <div class="form_input">
+                        <label for="description" class="form-label">{{ __('Description') }}</label>
+                        <textarea class="form-control @error('description') is-invalid @enderror" name="description" id="description"
+                            rows="5" minlength="5" maxlength="1000">{{ old('description', $dish->description) }}</textarea>
+                        @error('description')
+                            <div class="text-danger py-2">
+                                {{ $message }}
+                            </div>
+                        @enderror
+                    </div>
+
+                    <div class="form_input">
+                        <label for="ingredients" class="form-label">{{ __('Ingredients') }}</label>
+
+                        <textarea class="form-control @error('ingredients') is-invalid @enderror" minlength="5" maxlength="1000"
+                            name="ingredients" id="ingredients" rows="5" minlength="5" maxlength="1000">{{ old('ingredients', $dish->ingredients) }}</textarea>
+
+                        @error('ingredients')
+                            <div class="text-danger py-2">
+                                {{ $message }}
+                            </div>
+                        @enderror
+                    </div>
+
+                    <div class="check_flex">
+                        <div></div>
+                        <div class="d-flex align-items-end">
+                            <span>* </span>
+                            <h5> required fields</h5>
+                        </div>
+                    </div>
                 </div>
             </div>
-
-            <div class="mb-3">
-                <label for="description" class="form-label">{{ __('Description') }}</label>
-                <textarea class="form-control @error('description') is-invalid @enderror" name="description" id="description"
-                    rows="5" minlength="5" maxlength="1000">{{ old('description', $dish->description) }}</textarea>
-                @error('description')
-                    <div class="text-danger py-2">
-                        {{ $message }}
-                    </div>
-                @enderror
-            </div>
-
-            <div class="mb-3">
-                <label for="ingredients" class="form-label">{{ __('Ingredients') }}</label>
-
-                <textarea class="form-control @error('ingredients') is-invalid @enderror" minlength="5" maxlength="1000"
-                    name="ingredients" id="ingredients" rows="5" minlength="5" maxlength="1000">{{ old('ingredients', $dish->ingredients) }}</textarea>
-
-                @error('ingredients')
-                    <div class="text-danger py-2">
-                        {{ $message }}
-                    </div>
-                @enderror
-            </div>
-
-            <p class="text-secondary text-end">* = required fields </p>
-
-            <div class="d-flex justify-content-between">
-                <a href="{{ route('admin.dishes.index') }}" class="btn btn-secondary">
+            <div class="bottom">
+                <a href="{{ route('admin.dishes.index') }}" class="border-0 btn_dark gap-2 text-decoration-none">
                     <i class="fa fa-arrow-left" aria-hidden="true"></i> Back
                 </a>
-                <button type="submit" class="btn btn-success">
+                <button type="submit" class="border-0 btn_primary gap-2">
                     <i class="fa fa-check" aria-hidden="true"></i> Save
                 </button>
             </div>
