4cd3f86bd5d7063c980ef89c68cc27bd3c97545f
Daniele <danielecolaci@outlook.com>
2024-06-28 14:28:23 +0200

    Fix order show

--- resources/views/admin/orders/show.blade.php
+++ resources/views/admin/orders/show.blade.php
@@ -5,56 +5,34 @@
 @section('content')
 
     <div class="container mt-5">
-        <div class="card card-order text-white">
-            <div class="card-header text-white">
-                <h1 class="mb-0">{{ $order->restaurant->name }}</h1>
-            </div>
-            <div class="card-body">
-                <h2 class="my-4">{{ $order->customer_name }} {{ $order->customer_lastname }}</h2>
-                <div class="row">
-                    <div class="col-md-8">
-                        <ul class="list-unstyled lh-lg">
-                            <li><b>Address:</b> {{ $order->customer_address }}</li>
-                            <li><b>Email:</b> {{ $order->customer_email }}</li>
-                            <li><b>Phone:</b> {{ $order->customer_telephone }}</li>
-                        </ul>
-                    </div>
-                    <div class="col-md-4">
-                        <h5 class="fw-semibold">Order Summary</h5>
-
-                        <ul class="list-unstyled lh-lg dishes-list">
-
-                            @foreach ($order->dishes as $dish)
-                                {{-- <h6>@dd($dish->pivot->quantity)</h6> --}}
-
-                                <h6>Pz. {{ $dish->pivot->quantity }} {{ $dish->name }}</h6>
-                            @endforeach
-
-                        </ul>
-
-                        <ul class="list-unstyled lh-lg">
-                            <li><b>Total:</b> {{ $order->total }} &#8364;</li>
-                        </ul>
-
-                    </div>
+        <div class="py-4">
+            <h1 class="mb-0">{{ $order->restaurant->name }}</h1>
+            <hr class="my-4">
+            <h2 class="my-4">{{ $order->customer_name }} {{ $order->customer_lastname }}</h2>
+            <div class="row">
+                <div class="col-md-6">
+                    <ul class="list-unstyled">
+                        <li class="my-3"><b>Address:</b> {{ $order->customer_address }}</li>
+                        <li class="my-3"><b>Email:</b> {{ $order->customer_email }}</li>
+                        <li class="my-3"><b>Phone:</b> {{ $order->customer_telephone }}</li>
+                    </ul>
+                </div>
+                <div class="col-md-6">
+                    <h5 class="fw-bold mb-3">Order Summary</h5>
+                    <ul class="list-unstyled">
+                        @foreach ($order->dishes as $dish)
+                            <li class="my-2">
+                                <span class="badge bg-secondary">{{ $dish->pivot->quantity }}x</span>
+                                {{ $dish->name }}
+                            </li>
+                        @endforeach
+                    </ul>
+                    <ul class="list-unstyled">
+                        <li><b>Total:</b> {{ $order->total }} &#8364;</li>
+                    </ul>
                 </div>
             </div>
-
         </div>
-
-
-
-
-
-
     </div>
-    </div>
-
-    <div class="container">
-
-
-    </div>
-    </div>
-
 
 @endsection
