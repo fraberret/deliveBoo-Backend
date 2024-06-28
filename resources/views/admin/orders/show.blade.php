db1538c121052c6bedfcd1771076863bb5e6c61d
Daniele <danielecolaci@outlook.com>
2024-06-28 11:37:25 +0200

    Fix Order Show Card, remove order number

--- resources/views/admin/orders/show.blade.php
+++ resources/views/admin/orders/show.blade.php
@@ -5,17 +5,15 @@
 @section('content')
 
     <div class="container mt-5">
-        <div class="card">
-            <div class="card-header bg-primary text-white">
+        <div class="card card-order text-white">
+            <div class="card-header text-white">
                 <h1 class="mb-0">{{ $order->restaurant->name }}</h1>
             </div>
             <div class="card-body">
-                <h2 class="my-4">Order n.{{ $order->id }} Information</h2>
+                <h2 class="my-4">{{ $order->customer_name }} {{ $order->customer_lastname }}</h2>
                 <div class="row">
                     <div class="col-md-8">
                         <ul class="list-unstyled lh-lg">
-                            <li><b>Name:</b> {{ $order->customer_name }}</li>
-                            <li><b>Last Name:</b> {{ $order->customer_lastname }}</li>
                             <li><b>Address:</b> {{ $order->customer_address }}</li>
                             <li><b>Email:</b> {{ $order->customer_email }}</li>
                             <li><b>Phone:</b> {{ $order->customer_telephone }}</li>
@@ -25,15 +23,13 @@
                         <h5 class="fw-semibold">Order Summary</h5>
 
                         <ul class="list-unstyled lh-lg dishes-list">
-                            
+
                             @foreach ($order->dishes as $dish)
-                            
-                            {{-- <h6>@dd($dish->pivot->quantity)</h6> --}}
-                            
-                            <h6>Pz. {{$dish->pivot->quantity}} {{$dish->name}}</h6>
-                            
-                            @endforeach                            
-                            
+                                {{-- <h6>@dd($dish->pivot->quantity)</h6> --}}
+
+                                <h6>Pz. {{ $dish->pivot->quantity }} {{ $dish->name }}</h6>
+                            @endforeach
+
                         </ul>
 
                         <ul class="list-unstyled lh-lg">
@@ -44,20 +40,20 @@
                 </div>
             </div>
 
-            </div>
-
-
-
-
-
-
-            </div>
         </div>
 
-        <div class="container">
 
 
-        </div>
+
+
+
+    </div>
+    </div>
+
+    <div class="container">
+
+
+    </div>
     </div>
 
 
