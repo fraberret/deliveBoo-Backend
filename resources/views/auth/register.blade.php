70d4de5882466c1404ff37f8cb635dd8fa99d631
RiccardoImperiale <riccardoimperiale.dev@gmail.com>
2024-06-29 09:23:46 +0200

    add custom validation to register form and fix responsive

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -4,7 +4,6 @@
 
 @section('content')
     <div class="container">
-
         <form method="POST" class="register_form" id="registrationForm" action="{{ route('register') }}"
             enctype="multipart/form-data" style="max-width: 1140px">
             @csrf
@@ -17,63 +16,66 @@
                 </div>
 
                 <div class="inputs">
-
                     <div class="row row-cols-md-2">
+
+                        {{-- NAME  --}}
                         <div class="form_input">
                             <label for="name" class="col-form-label text-md-right">{{ __('Name*') }}</label>
-
                             <div>
                                 <input id="name" type="text"
                                     class="form-control @error('name') is-invalid @enderror" name="name"
                                     value="{{ old('name') }}" required pattern="^[A-Za-z0-9 ]+$"
                                     title="Only alphanumeric characters and spaces are allowed" minlength="2"
-                                    minlength="255" autocomplete="name" autofocus>
+                                    maxlength="255" autocomplete="name" autofocus>
 
                                 @error('name')
                                     <div class="alert alert-danger">{{ $message }}</div>
                                 @enderror
+                                <div class="custom_error name_error"></div>
                             </div>
                         </div>
-
+                        {{-- EMAIL --}}
                         <div class="form_input">
                             <label for="email" class="col-form-label text-md-right">{{ __('E-Mail Address*') }}</label>
 
                             <div>
                                 <input id="email" type="email"
                                     class="form-control @error('email') is-invalid @enderror" name="email"
-                                    value="{{ old('email') }}" required minlength="5" minlength="255"
+                                    value="{{ old('email') }}" required minlength="5" maxlength="255"
                                     autocomplete="email">
 
                                 @error('email')
                                     <div class="alert alert-danger">{{ $message }}</div>
                                 @enderror
+                                <div class="custom_error email_error"></div>
                             </div>
                         </div>
 
+                        {{-- PASSWORD --}}
                         <div class="form_input">
                             <label for="password" class="col-form-label text-md-right">{{ __('Password*') }}</label>
-
                             <div>
                                 <input id="password" type="password"
                                     class="form-control @error('password') is-invalid @enderror" name="password" required
-                                    minlength="6" maxlength="25" autocomplete="new-password">
+                                    minlength="8" maxlength="25" autocomplete="new-password">
                                 @error('password')
                                     <div class="alert alert-danger">{{ $message }}</div>
                                 @enderror
+                                <div class="custom_error password_error"></div>
                             </div>
                         </div>
-
+                        {{-- PASSWORD CONFIRM --}}
                         <div class="form_input">
                             <label for="password-confirm"
                                 class="col-form-label text-md-right">{{ __('Confirm Password*') }}</label>
 
                             <div>
                                 <input id="password-confirm" type="password" class="form-control"
-                                    name="password_confirmation" autocomplete="new-password">
+                                    name="password_confirmation" required autocomplete="new-password">
                             </div>
+                            <div class="custom_error confirm_password_error"></div>
                         </div>
                     </div>
-
 
                     <div class="title">
                         <h5>{{ __('Restaurant Info') }}</h5>
@@ -86,7 +88,7 @@
                             {{-- Restaurant Name --}}
                             <div class="form_input">
                                 <label for="restaurant_name"
-                                    class="col-form-label text-md-right">{{ __('Name*') }}</label>
+                                    class="col-form-label text-md-right">{{ __('Restaurant Name*') }}</label>
 
                                 <div>
                                     <input id="restaurant_name" type="text"
@@ -97,6 +99,8 @@
                                     @error('restaurant_name')
                                         <div class="alert alert-danger">{{ $message }}</div>
                                     @enderror
+                                    <div class="custom_error restaurant_name_error"></div>
+
                                 </div>
                             </div>
 
@@ -113,6 +117,8 @@
                                     @error('address')
                                         <div class="alert alert-danger">{{ $message }}</div>
                                     @enderror
+                                    <div class="custom_error address_error"></div>
+
                                 </div>
                             </div>
 
@@ -142,11 +148,13 @@
                                     <input id="piva" type="text"
                                         class="form-control @error('piva') is-invalid @enderror" name="piva"
                                         value="{{ old('piva') }}" required autocomplete="piva" autofocus
-                                        pattern="[0-9]{11}" title="Please enter exactly 11 digits for P.Iva">
+                                        pattern="[0-9]{11}" title="Please enter exactly 11 digits for VAT (P.Iva)">
 
                                     @error('piva')
                                         <div class="alert alert-danger">{{ $message }}</div>
                                     @enderror
+                                    <div class="custom_error vat_error"></div>
+
                                 </div>
                             </div>
                         </div>
@@ -186,6 +194,7 @@
                                 @error('cousines')
                                     <div class="alert alert-danger">{{ $message }}</div>
                                 @enderror
+                                <div class="custom_error cousines_error"></div>
 
                             </div>
                         </div>
@@ -210,29 +219,212 @@
                     </button>
                 </div>
         </form>
-
-
-
-
     </div>
 
     <script>
-        var password = document.getElementById("password")
-        let confirm_password = document.getElementById("password-confirm");
-        let email = document.getElementById("email")
+        const name = document.getElementById('name')
+        const email = document.getElementById("email")
+        const password = document.getElementById("password")
+        const confirm_password = document.getElementById("password-confirm");
+        const restaurantName = document.getElementById("restaurant_name");
+        const address = document.getElementById("address");
+        const vat = document.getElementById("piva");
+        const cousines = document.querySelector(".form_input_checkGroup ");
+
+
+        document.querySelector('button[type="submit"]').addEventListener('click', (event) => {
+            //     validateName()
+            //     validateEmail()
+            //     validatePassword()
+            //     confirmPassword()
+            //     validateRestaurantName()
+            //     validateAddress()
+            //     validateVat()
+            //     updateRequiredAttribute();
+            // }
+
+            if (validateName()) {
+                if (validateEmail()) {
+                    if (validatePassword()) {
+                        if (confirmPassword()) {
+                            if (validateRestaurantName()) {
+                                if (validateAddress()) {
+                                    if (validateVat()) {
+                                        updateRequiredAttribute();
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        });
+
+        const nameError = document.querySelector('.custom_error.name_error');
+        const emailError = document.querySelector('.custom_error.email_error');
+        const passwordError = document.querySelector('.custom_error.password_error');
+        const confirmPasswordError = document.querySelector('.custom_error.confirm_password_error');
+        const restaurantNameError = document.querySelector('.custom_error.restaurant_name_error');
+        const addressError = document.querySelector('.custom_error.address_error');
+        const vatError = document.querySelector('.custom_error.vat_error');
+        const cousinesError = document.querySelector('.custom_error.cousines_error');
+
+
+        function validateName() {
+            if (name.validity.valueMissing) {
+                nameError.textContent = 'Name is required.';
+                name.style.borderColor = '#fb4848'
+                nameError.style.display = 'block';
+                return false;
+            } else if (name.validity.patternMismatch) {
+                nameError.textContent = 'Only alphanumeric characters and spaces are allowed.';
+                name.style.borderColor = '#fb4848'
+                nameError.style.display = 'block';
+                return false;
+            } else if (name.validity.tooShort || name.validity.tooLong) {
+                nameError.textContent = 'Name must be between 2 and 255 characters.';
+                name.style.borderColor = '#fb4848'
+                nameError.style.display = 'block';
+                return false;
+            } else {
+                name.style.borderColor = ''
+                nameError.style.display = 'none';
+                return true;
+            }
+        }
+
+        function validateEmail() {
+            let emailPattern = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+
+            if (email.validity.valueMissing) {
+                emailError.textContent = 'Email is required.';
+                email.style.borderColor = '#fb4848'
+                emailError.style.display = 'block';
+                return false;
+            } else if (!emailPattern.test(email.value)) {
+                emailError.textContent = 'Please enter a valid email address.';
+                email.style.borderColor = '#fb4848'
+                emailError.style.display = 'block';
+                return false;
+            } else if (email.validity.tooShort || email.validity.tooLong) {
+                emailError.textContent = 'Email must be between 5 and 255 characters.';
+                email.style.borderColor = '#fb4848'
+                emailError.style.display = 'block';
+                return false;
+            } else {
+                email.style.borderColor = ''
+                emailError.style.display = 'none';
+                return true;
+            }
+        }
+
+        function validatePassword() {
+            if (password.validity.valueMissing) {
+                passwordError.textContent = 'Password is required.';
+                password.style.borderColor = '#fb4848'
+                passwordError.style.display = 'block';
+                return false;
+            } else if (password.validity.tooShort || password.validity.tooLong) {
+                passwordError.textContent = 'Password must be between 8 and 25 characters.';
+                password.style.borderColor = '#fb4848'
+                passwordError.style.display = 'block';
+                return false;
+            } else {
+                password.style.borderColor = ''
+                passwordError.style.display = 'none';
+                return true;
+            }
+        }
+
+        function confirmPassword() {
+            if (confirm_password.validity.valueMissing) {
+                confirmPasswordError.textContent = 'This field is required.';
+                confirm_password.style.borderColor = '#fb4848'
+                confirmPasswordError.style.display = 'block';
+                return false;
+            } else if (password.value !== confirm_password.value) {
+                confirmPasswordError.textContent = "Password doesn't match.";
+                confirm_password.style.borderColor = '#fb4848'
+                confirmPasswordError.style.display = 'block';
+                return false;
+            } else {
+                confirm_password.style.borderColor = ''
+                confirmPasswordError.style.display = 'none';
+                return true;
+            }
+        }
+
+        function validateRestaurantName() {
+            if (restaurantName.validity.valueMissing) {
+                restaurantNameError.textContent = 'A restaurant name is required.';
+                restaurantName.style.borderColor = '#fb4848'
+                restaurantNameError.style.display = 'block';
+                return false;
+            } else if (restaurantName.validity.tooShort || restaurantName.validity.tooLong) {
+                restaurantNameError.textContent = 'Name must be between 2 and 50 characters.';
+                restaurantName.style.borderColor = '#fb4848'
+                restaurantNameError.style.display = 'block';
+                return false;
+            } else {
+                restaurantName.style.borderColor = ''
+                restaurantNameError.style.display = 'none';
+                return true;
+            }
+        }
+
+        function validateAddress() {
+            if (address.validity.valueMissing) {
+                addressError.textContent = 'The address is required.';
+                address.style.borderColor = '#fb4848'
+                addressError.style.display = 'block';
+                return false;
+            } else if (address.validity.tooShort || address.validity.tooLong) {
+                addressError.textContent = 'address must be between 5 and 255 characters.';
+                address.style.borderColor = '#fb4848'
+                addressError.style.display = 'block';
+                return false;
+            } else {
+                address.style.borderColor = ''
+                addressError.style.display = 'none';
+                return true;
+            }
+        }
+
+        function validateVat() {
+            if (vat.validity.valueMissing) {
+                vatError.textContent = 'Vat required.';
+                vat.style.borderColor = '#fb4848'
+                vatError.style.display = 'block';
+                return false;
+            } else if (vat.validity.patternMismatch) {
+                vatError.textContent = 'Please enter exactly 11 digits for VAT.';
+                vat.style.borderColor = '#fb4848'
+                vatError.style.display = 'block';
+                return false;
+            } else {
+                vat.style.borderColor = ''
+                vatError.style.display = 'none';
+                return true;
+            }
+        }
+
         const checkboxes = document.querySelectorAll('input[name="cousines[]"]');
 
-        let checkedBoxes = 0
+        let checkedBoxes = 0;
 
         function updateRequiredAttribute() {
             checkboxes.forEach(checkbox => {
                 if (checkedBoxes > 0) {
                     checkbox.removeAttribute('required');
                     checkbox.setCustomValidity('');
-
+                    cousines.style.borderColor = ''
+                    cousinesError.style.display = 'none';
                 } else {
                     checkbox.setAttribute('required', 'required');
                     checkbox.setCustomValidity('Please select at least one option.');
+                    cousinesError.textContent = 'Please select at least one option.';
+                    cousines.style.borderColor = '#fb4848'
+                    cousinesError.style.display = 'block';
                 }
             });
         };
@@ -249,32 +441,6 @@
                 updateRequiredAttribute();
             });
         });
-
-
-        function validatePassword() {
-
-            if (password.value != confirm_password.value) {
-                confirm_password.setCustomValidity("Passwords Don't Match");
-            } else {
-                confirm_password.setCustomValidity('');
-            }
-        }
-
-        function validateEmail() {
-            let emailPattern = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
-
-            if (emailPattern.test(email.value)) {
-                email.setCustomValidity("");
-            } else {
-                email.setCustomValidity("Please enter a valid email address");
-            }
-        }
-
-        password.onchange = validatePassword;
-        confirm_password.onkeyup = validatePassword;
-
-        email.onchange = validateEmail;
-        email.onkeyup = validateEmail;
     </script>
 
 @endsection
